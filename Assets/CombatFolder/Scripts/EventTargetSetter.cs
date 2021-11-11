5eb40846814eb0220c368ef81bfca88ff4769b73
PAPAYA <dameningen@126.com>
2021-11-10 20:27:32 -0500

    enemy in integrated scene + dialogue trigger spawner

---
+++ Assets/CombatFolder/Scripts/EventTargetSetter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTargetSetter : MonoBehaviour
+{
+    public List<Transform> pos;
+
+	private void Update()
+	{
+		if (pos.Count == 2)
+		{
+			transform.position = pos[GameManager.me.stateOfLevel].position;
+		}
+		else
+		{
+			Debug.LogError("there should be two pos, for two ai break-ins");
+		}
+	}
+}
