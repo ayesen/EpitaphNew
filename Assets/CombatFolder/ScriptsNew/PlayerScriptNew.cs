a4f76d0cf9a1890a98ee80a70f0ae150255763b0
PAPAYA <dameningen@126.com>
2021-11-29 15:15:15 -0500

    hurt and kill player

--- Assets/CombatFolder/ScriptsNew/PlayerScriptNew.cs
+++ Assets/CombatFolder/ScriptsNew/PlayerScriptNew.cs
@@ -13,6 +13,7 @@ public class PlayerScriptNew : MonoBehaviour
 	private GameObject enemy;
     private Animator anim;
     public GameObject actualPlayer;
+	private bool dead = false;
 	[Header("Mat")]
 	public List<GameObject> selectedMats; // mats activated
 	public List<GameObject> matSlots; // inventory
@@ -40,183 +41,198 @@ public class PlayerScriptNew : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.T))
 		{
-			GetComponentInChildren<Rigidbody>().AddForce(new Vector3(1,0,1) * 50, ForceMode.Impulse);
+			//GetComponentInChildren<Rigidbody>().AddForce(new Vector3(1,0,1) * 50, ForceMode.Impulse);
 		}
-		// check for attack button press
-		if (selectedMats.Count > 0 &&  // check if player has mat activated
-			(anim.GetCurrentAnimatorStateInfo(0).IsName("testIdle") || // if player in idle state
-			anim.GetCurrentAnimatorStateInfo(0).IsName("testWalk")))  // if player in walk state
+		Death();
+		if (!dead)
 		{
-			if (Input.GetMouseButtonUp(0)) // if left click
+			// check for attack button press
+			if (selectedMats.Count > 0 &&  // check if player has mat activated
+				(anim.GetCurrentAnimatorStateInfo(0).IsName("testIdle") || // if player in idle state
+				anim.GetCurrentAnimatorStateInfo(0).IsName("testWalk")))  // if player in walk state
 			{
-				bool goodToGo = true;
-				foreach (var mat in selectedMats)
+				if (Input.GetMouseButtonUp(0)) // if left click
 				{
-					if (mat.GetComponent<MatScriptNew>().amount <= 0)
-					{
-						goodToGo = false;
-					}
-				}
-				if (goodToGo)
-				{
+					bool goodToGo = true;
 					foreach (var mat in selectedMats)
 					{
-						mat.GetComponent<MatScriptNew>().amount--;
+						if (mat.GetComponent<MatScriptNew>().amount <= 0)
+						{
+							goodToGo = false;
+						}
 					}
-					anim.Play("testWindup"); // player anticipation clip and call effect manager's casting event in clip
+					if (goodToGo)
+					{
+						foreach (var mat in selectedMats)
+						{
+							mat.GetComponent<MatScriptNew>().amount--;
+						}
+						anim.Play("testWindup"); // player anticipation clip and call effect manager's casting event in clip
+					}
+					else
+					{
+						print("YOU DON'T HAVE ENOUGH MATERIALS!!!");
+					}
+				}
+			}
+			#region Temp UI
+			if (selectedMats.Contains(matSlots[0]))
+			{
+				mat1.text = matSlots[0].name + ": " + matSlots[0].GetComponent<MatScriptNew>().amount;
+			}
+			else
+			{
+				mat1.text = "mat 1 not selected";
+			}
+			if (selectedMats.Contains(matSlots[1]))
+			{
+				mat2.text = matSlots[1].name + ": " + matSlots[1].GetComponent<MatScriptNew>().amount;
+			}
+			else
+			{
+				mat2.text = "mat 2 not selected";
+			}
+			if (selectedMats.Contains(matSlots[2]))
+			{
+				mat3.text = matSlots[2].name + ": " + matSlots[2].GetComponent<MatScriptNew>().amount;
+			}
+			else
+			{
+				mat3.text = "mat 3 not selected";
+			}
+			if (selectedMats.Contains(matSlots[3]))
+			{
+				mat4.text = matSlots[3].name + ": " + matSlots[3].GetComponent<MatScriptNew>().amount;
+			}
+			else
+			{
+				mat4.text = "mat 4 not selected";
+			}
+			#endregion
+			#region activate and deactivate mats
+			// activate mats
+			if (Input.GetKeyUp(KeyCode.Alpha1))
+			{
+				if (selectedMats.Contains(matSlots[0]))
+				{
+					selectedMats.Remove(matSlots[0]);
 				}
 				else
 				{
-					print("YOU DON'T HAVE ENOUGH MATERIALS!!!");
+					selectedMats.Add(matSlots[0]);
+				}
+				EffectManagerNew.me.RefreshCurrentMats();
+			}
+			else if (Input.GetKeyUp(KeyCode.Alpha2))
+			{
+				if (selectedMats.Contains(matSlots[1]))
+				{
+					selectedMats.Remove(matSlots[1]);
+				}
+				else
+				{
+					selectedMats.Add(matSlots[1]);
+				}
+				EffectManagerNew.me.RefreshCurrentMats();
+			}
+			else if (Input.GetKeyUp(KeyCode.Alpha3))
+			{
+				if (selectedMats.Contains(matSlots[2]))
+				{
+					selectedMats.Remove(matSlots[2]);
+				}
+				else
+				{
+					selectedMats.Add(matSlots[2]);
+				}
+				EffectManagerNew.me.RefreshCurrentMats();
+			}
+			else if (Input.GetKeyUp(KeyCode.Alpha4))
+			{
+				if (selectedMats.Contains(matSlots[3]))
+				{
+					selectedMats.Remove(matSlots[3]);
+				}
+				else
+				{
+					selectedMats.Add(matSlots[3]);
+				}
+				EffectManagerNew.me.RefreshCurrentMats();
+			}
+			#endregion
+			#region movement
+			// simple movement for now
+			if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) &&
+				anim.GetCurrentAnimatorStateInfo(0).IsName("testIdle")) // if in idel state and a movement key pressed, go into walk state
+			{
+				anim.Play("testWalk");
+			}
+			if (anim.GetCurrentAnimatorStateInfo(0).IsName("testWalk")) // if in walk state, walk
+			{
+				// walking diagonally
+				if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+				{
+					transform.position = new Vector3(transform.position.x - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+				{
+					transform.position = new Vector3(transform.position.x + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+				{
+					transform.position = new Vector3(transform.position.x - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+				{
+					transform.position = new Vector3(transform.position.x + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
+				}
+				// walking in one axis
+				else if (Input.GetKey(KeyCode.W))
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + spd * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.S))
+				{
+					transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - spd * Time.deltaTime);
+				}
+				else if (Input.GetKey(KeyCode.A))
+				{
+					transform.position = new Vector3(transform.position.x - spd * Time.deltaTime, transform.position.y, transform.position.z);
+				}
+				else if (Input.GetKey(KeyCode.D))
+				{
+					transform.position = new Vector3(transform.position.x + spd * Time.deltaTime, transform.position.y, transform.position.z);
+				}
+				else
+				{
+					anim.Play("testIdle");
 				}
 			}
-		}
-		#region Temp UI
-		if (selectedMats.Contains(matSlots[0]))
-		{
-			mat1.text = matSlots[0].name + ": " + matSlots[0].GetComponent<MatScriptNew>().amount;
-		}
-		else
-		{
-			mat1.text = "mat 1 not selected";
-		}
-		if (selectedMats.Contains(matSlots[1]))
-		{
-			mat2.text = matSlots[1].name + ": " + matSlots[1].GetComponent<MatScriptNew>().amount;
-		}
-		else
-		{
-			mat2.text = "mat 2 not selected";
-		}
-		if (selectedMats.Contains(matSlots[2]))
-		{
-			mat3.text = matSlots[2].name + ": " + matSlots[2].GetComponent<MatScriptNew>().amount;
-		}
-		else
-		{
-			mat3.text = "mat 3 not selected";
-		}
-		if (selectedMats.Contains(matSlots[3]))
-		{
-			mat4.text = matSlots[3].name + ": " + matSlots[3].GetComponent<MatScriptNew>().amount;
-		}
-		else
-		{
-			mat4.text = "mat 4 not selected";
-		}
-		#endregion
-		#region activate and deactivate mats
-		// activate mats
-		if (Input.GetKeyUp(KeyCode.Alpha1))
-		{
-			if (selectedMats.Contains(matSlots[0]))
-			{
-				selectedMats.Remove(matSlots[0]);
-			}
-			else
-			{
-				selectedMats.Add(matSlots[0]);
-			}
-			EffectManagerNew.me.RefreshCurrentMats();
-		}
-		else if (Input.GetKeyUp(KeyCode.Alpha2))
-		{
-			if (selectedMats.Contains(matSlots[1]))
-			{
-				selectedMats.Remove(matSlots[1]);
-			}
-			else
-			{
-				selectedMats.Add(matSlots[1]);
-			}
-			EffectManagerNew.me.RefreshCurrentMats();
-		}
-		else if (Input.GetKeyUp(KeyCode.Alpha3))
-		{
-			if (selectedMats.Contains(matSlots[2]))
-			{
-				selectedMats.Remove(matSlots[2]);
-			}
-			else
-			{
-				selectedMats.Add(matSlots[2]);
-			}
-			EffectManagerNew.me.RefreshCurrentMats();
-		}
-		else if (Input.GetKeyUp(KeyCode.Alpha4))
-		{
-			if (selectedMats.Contains(matSlots[3]))
-			{
-				selectedMats.Remove(matSlots[3]);
-			}
-			else
-			{
-				selectedMats.Add(matSlots[3]);
-			}
-			EffectManagerNew.me.RefreshCurrentMats();
-		}
-		#endregion
-		#region movement
-		// simple movement for now
-		if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) &&
-			anim.GetCurrentAnimatorStateInfo(0).IsName("testIdle")) // if in idel state and a movement key pressed, go into walk state
-		{
-			anim.Play("testWalk");
-		}
-		if (anim.GetCurrentAnimatorStateInfo(0).IsName("testWalk")) // if in walk state, walk
-		{
-			// walking diagonally
-			if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-			{
-				transform.position = new Vector3(transform.position.x - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-			{
-				transform.position = new Vector3(transform.position.x + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-			{
-				transform.position = new Vector3(transform.position.x - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-			{
-				transform.position = new Vector3(transform.position.x + Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime, transform.position.y, transform.position.z - Mathf.Sqrt(Mathf.Pow(spd, 2) / 2) * Time.deltaTime);
-			}
-			// walking in one axis
-			else if (Input.GetKey(KeyCode.W))
-			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + spd * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.S))
-			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - spd * Time.deltaTime);
-			}
-			else if (Input.GetKey(KeyCode.A))
-			{
-				transform.position = new Vector3(transform.position.x - spd * Time.deltaTime, transform.position.y, transform.position.z);
-			}
-			else if (Input.GetKey(KeyCode.D))
-			{
-				transform.position = new Vector3(transform.position.x + spd * Time.deltaTime, transform.position.y, transform.position.z);
-			}
-			else
-			{
-				anim.Play("testIdle");
-			}
-		}
 
-		// look at mouse pos(not changing y-axis)
-		//! if this doesn't work properly, check game objects' layers, and make sure the mouse manager ignores the proper layers
-		if (Input.GetMouseButton(1))
-		{
-			var target = new Vector3(enemy.transform.position.x, transform.position.y, enemy.transform.position.z);
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target - transform.position), rot_spd * Time.deltaTime);
+			// look at mouse pos(not changing y-axis)
+			//! if this doesn't work properly, check game objects' layers, and make sure the mouse manager ignores the proper layers
+			if (Input.GetMouseButton(1))
+			{
+				var target = new Vector3(enemy.transform.position.x, transform.position.y, enemy.transform.position.z);
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target - transform.position), rot_spd * Time.deltaTime);
+			}
+			else
+			{
+				var target = new Vector3(MouseManager.me.mousePos.x, transform.position.y, MouseManager.me.mousePos.z);
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target - transform.position), rot_spd * Time.deltaTime);
+			}
+			#endregion
 		}
-		else
+	}
+	public void LoseHealth_player(int amt)
+	{
+		hp -= amt;
+	}
+	public void Death()
+	{
+		if (hp <= 0)
 		{
-			var target = new Vector3(MouseManager.me.mousePos.x, transform.position.y, MouseManager.me.mousePos.z);
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target - transform.position), rot_spd * Time.deltaTime);
+			dead = true;
 		}
-		#endregion
 	}
 }
